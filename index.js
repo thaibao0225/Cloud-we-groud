03644a3a811e06b216e5b9b3e68d838421538e8a
MSI GF63 Thin 95C <you@example.com>
2020-06-01 16:23:11 +0700

    hello hiho

--- index.js
+++ index.js
@@ -2,12 +2,42 @@ var express = require("express");
 var app = express();
 app.set("view engine", "ejs");
 app.set("views", "./views");
-app.listen(process.env.PORT || 3000);
+
 
 app
 .get("/", function(req, res)
 {
 res.render("HenshinShop")
 });
-app.set("view engine", "css");
-app.use(express.static(path.join(__dirname, 'public')));
+
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+
+
+
+    function onRequest(request, response){  
+        if(request.headers.accept.split(',')[0] == 'text/css') {
+             console.log('TRUE');
+
+             fs.readFile('index.css', (err, data)=>{
+                 response.writeHeader(200, {'Content-Type': 'text/css'});
+                 response.write(data);
+                 response.end();
+             });  
+        }
+
+        else {
+            console.log('FALSE');    
+
+            fs.readFile('index.html', function(err, data){
+                response.writeHead(200, {'Content_type': 'text/html'});
+                response.write(data);
+                response.end();
+            });
+        };
+    };
+
+    http.createServer(onRequest).listen(8888);
+    console.log('[SERVER] - Started!');
