36eff559917b263d35325fb4374c384edfe3040e
MSI GF63 Thin 95C <you@example.com>
2020-06-01 17:02:28 +0700

    zhinchaooo

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require("express");
  var app = express();
  app.set("view engine", "ejs");
+
+ app.use('/assets', express.static('assets'));
+
  app.set("views","./views");
  app.listen(process.env.PORT || 3000);
 
