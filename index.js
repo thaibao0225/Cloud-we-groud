1d217a3b6f8ecefd7ac0f14c94a738faa9e3c0f5
MSI GF63 Thin 95C <you@example.com>
2020-06-01 16:08:04 +0700

    hello mn

--- index.js
+++ index.js
@@ -9,3 +9,17 @@ app
 {
 res.render("HenshinShop")
 });
+
+
+var http = require('http');
+     var fs = require('fs');
+
+ //creating server
+ http.createServer(function (req, res) {
+   fs.readFile('LandingPage.html', function (err, html) {
+     res.writeHead(200, { 'Content-Type': 'text/html' });
+     res.write(html);
+     res.end();
+
+   });
+ }).listen(8080);
