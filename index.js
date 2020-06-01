5b8c3f31d83398b9e7b634516b5dc29f6ab0d6d1
MSI GF63 Thin 95C <you@example.com>
2020-06-01 17:55:05 +0700

    hquaolmo

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ var express = require("express");
  var app = express();
  app.set("view engine", "ejs");
  app.set("views","./views");
- app.listen(process.env.PORT || 3000);
+ 
 
  app.get("/", function(reg,res){
  	res.render("HenshinShop");
  });
  //load view Engine
- app.set('views', path.join(__dirname, 'views'));
+ app.use(express.static('public'));
+ app.set('views', path.join(__dirname, './views'));
  app.set('view engine', 'pug');
  res.render("index");
+ app.listen(process.env.PORT || 3000);
