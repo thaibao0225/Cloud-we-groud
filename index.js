68207448964aa0c2f9e1e353c6c0758c8078eeef
MSI GF63 Thin 95C <you@example.com>
2020-06-01 18:24:37 +0700

    shiffft

--- index.js
+++ index.js
@@ -12,8 +12,4 @@ var express = require("express");
  //load view Engine
  app.use(express.static('public'));
  app.set('views', path.join(__dirname, './views'));
-
-
-
- app.set('views', imagepath.join(__dirname, './views'));
  app.listen(process.env.PORT || 3000);
