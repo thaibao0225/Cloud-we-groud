1938cca4d957ac2d0e972e84d7f105e42c414f29
MSI GF63 Thin 95C <you@example.com>
2020-06-01 16:31:56 +0700

    hquaoo

--- index.js
+++ index.js
@@ -11,34 +11,3 @@ res.render("HenshinShop")
 });
 app.use('/public',express.static('public'));
 app.use('/css',express.static(__dirname +'/css'));
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-
-
-
-
-    function onRequest(request, response){  
-        if(request.headers.accept.split(',')[0] == 'text/css') {
-             console.log('TRUE');
-
-             fs.readFile('index.css', (err, data)=>{
-                 response.writeHeader(200, {'Content-Type': 'text/css'});
-                 response.write(data);
-                 response.end();
-             });  
-        }
-
-        else {
-            console.log('FALSE');    
-
-            fs.readFile('index.html', function(err, data){
-                response.writeHead(200, {'Content_type': 'text/html'});
-                response.write(data);
-                response.end();
-            });
-        };
-    };
-
-    http.createServer(onRequest).listen(8888);
-    console.log('[SERVER] - Started!');
