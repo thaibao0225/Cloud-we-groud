3aac3cb39e44c36f617a018e78826498d36401b7
MSI GF63 Thin 95C <you@example.com>
2020-06-29 14:08:04 +0700

    first commit

--- index.js
+++ index.js
@@ -22,6 +22,9 @@ app.get("/taotaikhoan", function(req,res){
 	res.render("page/cre-account");
 });
 
+app.get("/login", function(req,res){
+	res.render("page/login");
+});
 
 app.get("/account", function(req,res){
 	res.render("page/mange-account");
@@ -40,9 +43,7 @@ app.get("/product", function(req,res){
 });
 
 
-////////////////////
-
-////////////////////////////////
+////////////////////////////////////////////////////
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -63,7 +64,7 @@ app.post('/taotaikhoan', function (req, res) {
 			});
 	// ...
   });
-  //////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////
   app.post('/login', function (req, res) {
 	var body = req.body;
 	var us = body.username;
@@ -73,90 +74,17 @@ app.post('/taotaikhoan', function (req, res) {
 				if (err) throw err;
 				var dbo = db.db("Thunghiem");
 				var myobj = {ID : us, Pass: pd};
-				dbo.collection("User").findOne(myobj, function(err, res) {
+				dbo.collection("User").find(myobj, function(err, res) {
 				  if (err) throw err;
-				  console.log("1 document inserted");
+				  console.log("1");
 				  db.close();
 				});
 			});
 	// ...
   });
-  ///////////////////////////////////////////////////////
-  
-// app.post('/taotaikhoan', (req, res) => {
-	
-// 	const username = req.query.username;
-// 	const password = req.query.password;
-//     console.log(body);
-// 	 let userInput = {
-// 		username: username,
-// 		password: password
-// 	 };
-// 	 console.log(userInput);
-
-// 	  MongoClient.connect(uri,{ useNewUrlParser: true , useUnifiedTopology: true } , function(err, db) {
-// 		if (err) throw err;
-// 		var dbo = db.db("Thunghiem");
-// 		var myobj = {ID : username, Pass: password};
-// 		dbo.collection("User").insertOne(myobj, function(err, res) {
-// 		  if (err) throw err;
-// 		  console.log("1 document inserted");
-// 		  db.close();
-// 		});
-// 	  });
-//   });
-  
-	
-		
-  
-  
-////////////////find///////////////
-
-////////////////////insert/////////////////////////
-// function insertDB()
-// { 
-//   MongoClient.connect(uri, function(err, db) {
-//     if (err) throw err;
-//     var dbo = db.db("ATNCompany");
-//     var myobj = {Product_Id:"3", Product_Name: "Nhune", Discription: "leuleuleu" , Price:"60$", Product_Img:"ban001.jpg"};
-//     dbo.collection("Product").insertOne(myobj, function(err, res) {
-//       if (err) throw err;
-//       console.log("1 document inserted");
-//       db.close();
-//     });
-//   });
-// }
-// insertDB();
-// /// ***************** ***************** *****************
-
-// /////////////////////////////////
-// function viewDB(req, res){
-// 	MongoClient.connect(
-// 		uri, 
-// 		{ useNewUrlParser: true , useUnifiedTopology: true }
-// 	)
-// 	.then (client => {
-// 	  var dbo = client.db(NameDataBase);
-// 	////// Điều kiện truy vấn 
-// 	  /// <, <=, >, >=, != 
-// 	///// $lt, $lte, $gt, $gte, $ne
-// 	// var vQuery = {
-// 	// 	User_name: /.*u.*/
-// 		//User_id : { $gte : 1 }
-// 		// };
-// 		var vQuery = {  Product_Id : { $gte : 1 } };	
-// 		dbo.collection(NameTable).find(vQuery).toArray()
-// 		.then (results => {
-// 			console.log(results);
-// 			client.close();
-// 		})
-// 		.catch(error => console.error(error));
-// 	})
-// 	.catch(error => console.error(error));
-// }
-// viewDB();
- //load view Engine
+  //////////////////////////////////////////////////
+ 
  app.use(express.static('public'));//
  app.set('views', path.join(__dirname, './views'));
- app.listen(8080);
+ app.listen(5000);
  
